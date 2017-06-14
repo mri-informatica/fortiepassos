2452ee49cbebf43f83d5de728d766e483c692471
mri-informatica <edson@mri-informatica.com>
2017-06-14 13:36:02 -0300

    envio
    envio de repositório

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'fortie-passos',
+		wp_ws_url : 'https://fortiepassos.com/wp-appkit-api/fortie-passos',
+		wp_url : 'https://fortiepassos.com',
+		theme : 'q-ios',
+		version : '0.1',
+		app_title : 'Fortie Profissional',
+		app_platform : 'ios',
+		gmt_offset : -3,
+		debug_mode : 'off',
+		auth_key : 'nwr9dCWnf5A3fpDqRzMSrBWNQgpRv7DHUYUlNgixFU0K37AQoQgaoykD0QfboqXm',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
